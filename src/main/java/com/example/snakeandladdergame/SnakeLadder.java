688f14de7eb4bb19d8c263ac4e4c884bf2a5217d
unknown <mannmanjeet988@gmail.com>
2022-12-22 12:02:32 +0530

    Added BoardImage

--- src/main/java/com/example/snakeandladdergame/SnakeLadder.java
+++ src/main/java/com/example/snakeandladdergame/SnakeLadder.java
@@ -10,57 +10,62 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-
-
-
+import java.io.InputStream;
 
 public class SnakeLadder extends Application {
 
     public static final int tileSize=40, height = 10, width = 10;
 
-    Pane createContent(){
+    Pane createContent() {
         Pane root = new Pane();
-        root.setPrefSize(width*tileSize,height*tileSize+50);
+        root.setPrefSize(width * tileSize, height * tileSize + 50);
 
-        int lowerLine=tileSize*height;
+        int lowerLine = tileSize * height;
 
         Player firstPlayer = new Player(tileSize, Color.BLACK, "Amit");
-        Player secondPlayer = new Player(tileSize-10, Color.WHITE, "Sumit");
+        Player secondPlayer = new Player(tileSize - 10, Color.WHITE, "Sumit");
 
-
-        for(int i=0; i < width;i++){
-            for(int j=0; j < height;j++){
-                Tile tile= new Tile(tileSize);
-                tile.setTranslateX(j*tileSize);
-                tile.setTranslateY(i*tileSize);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Tile tile = new Tile(tileSize);
+                tile.setTranslateX(j * tileSize);
+                tile.setTranslateY(i * tileSize);
                 root.getChildren().add(tile);
             }
         }
         root.getChildren().add(new Tile(tileSize));
 
-        Button playerOneButton= new Button("Player One");playerOneButton.setTranslateX(20);
-        playerOneButton.setTranslateY(lowerLine+20);
-        Button playerTwoButton= new Button("Player Two");
+        Button playerOneButton = new Button("Player One");
+        playerOneButton.setTranslateX(20);
+        playerOneButton.setTranslateY(lowerLine + 20);
+        Button playerTwoButton = new Button("Player Two");
 
         playerTwoButton.setTranslateX(250);
-        playerTwoButton.setTranslateY(lowerLine+20);
-        root.getChildren().addAll(playerOneButton,playerTwoButton,firstPlayer.getCoin(),secondPlayer.getCoin());
+        playerTwoButton.setTranslateY(lowerLine + 20);
 
-       // Image img=new Image(newFileInputStream(path));
 //        Image img=new Image(newFileInputStream("I:\\SnakeAndLadderGame\\src\\main\\resources\\SnakeLadderBoard12Nov.jpg"));
 //        ImageView boardImage = new ImageView();
 //        boardImage.setImage(img);
-//        boardImage.setFitWidth(tileSize*width);
-//        boardImage.setFitHeight(tileSize*height);
 //        root.getChildren().add(boardImage);
+
+        InputStream stream1 = null;
+        try {
+            stream1 = new FileInputStream("I:\\SnakeAndLadderGame\\src\\main\\resources\\SnakeLadderBoard12Nov.jpg");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Image image = new Image(stream1);
+        ImageView boardImage = new ImageView();
+
+        boardImage.setImage(image);
+        boardImage.setFitWidth(tileSize * width);
+        boardImage.setFitHeight(tileSize * height);
+
+        root.getChildren().addAll(boardImage, playerOneButton, playerTwoButton, firstPlayer.getCoin(), secondPlayer.getCoin());
         return root;
     }
-
-//    private String newFileInputStream(String s) {
-//        String path= "I:\\SnakeAndLadderGame\\src\\main\\resources\\SnakeLadderBoard12Nov.jpg";
-//        return path;
-//    }
 
     @Override
     public void start(Stage stage) throws IOException {
